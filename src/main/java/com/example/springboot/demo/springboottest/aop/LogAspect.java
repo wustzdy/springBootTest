d151501d8d5fe262578484b1f9fa39df357194ce
wustzdy <714403855@qq.com>
2020-07-26 10:59:03 +0800

    add test aop

--- src/main/java/com/example/springboot/demo/springboottest/aop/LogAspect.java
+++ src/main/java/com/example/springboot/demo/springboottest/aop/LogAspect.java
@@ -38,72 +38,21 @@ public class LogAspect {
      * ~ 第四个 * 任意方法
      * ~ .. 匹配任意数量的参数.
      */
-    @Pointcut("(execution(public * com.example.springboot.demo.springboottest.controller.*(..)))")
+   /* @Pointcut("(execution(public * com.example.springboot.demo.springboottest.controller.*(..)))")
     public void webLog() {
+    }*/
+
+    //Controller层切点
+    @Pointcut("execution (public * com.example.springboot.demo.springboottest.controller..*.*(..))")
+    public void controllerAspect() {
     }
 
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) throws UnsupportedEncodingException {
-        startTime.set(System.currentTimeMillis());
-
-        // 接收到请求，记录请求内容
-        log.info("========================= before -- start =========================");
-        Signature signature = joinPoint.getSignature();
-        MethodSignature methodSignature = (MethodSignature) signature;
-
-        log.info("CLASS_METHOD : " + methodSignature.getDeclaringTypeName() + "." + methodSignature.getName());
-
-        log.info("请求参数:    名称     值");
-        String[] argsNameArray = methodSignature.getParameterNames();
-        Object[] argsValueArray = joinPoint.getArgs();
-        if (argsNameArray == null || argsNameArray.length < 1) {
-            log.info("args_name: null");
-            log.info("args_value: null");
-        } else {
-            for (int i = 0; i < argsNameArray.length; i++) {
-                log.info("args_name: " + argsNameArray[i]);
-
-                String argValue = argsValueArray[i] != null ? argsValueArray[i].toString() : "";
-                if (argsNameArray[i].contains("encode")) {
-                    String str = URLDecoder.decode(argValue, "utf-8");
-                    log.info("args_value: " + (str.length() > 200 ? str.substring(0, 200) + "..." : str));
-                } else {
-                    log.info("args_value: " + (argValue.length() > 200 ? argValue.substring(0, 200) + "..." : argValue));
-                }
-            }
+    @Before("controllerAspect()")
+    public void doBefore(JoinPoint joinPoint) {
+        log.info("请求钱before。。。。。。。。。。。。。。。。。。。。。");
+        /*用isDebugEnabled方法判断下是能提升性能的*/
+        if (log.isInfoEnabled()) {
+            log.info("before " + joinPoint);
         }
-
-        // 记录下请求内容
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if (attributes == null) {
-            log.info("========================= before -- end =========================");
-            return;
-        }
-
-        HttpServletRequest request = attributes.getRequest();
-        log.info("IP : " + request.getRemoteAddr());
-
-        log.info("========================= before -- end =========================");
-    }
-
-    @AfterReturning(returning = "rvt", pointcut = "webLog()")
-    public void doAfterReturning(JoinPoint joinPoint, Object rvt) {
-        log.info("========================= after -- start =========================");
-        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName()
-                + "." + joinPoint.getSignature().getName());
-
-        if (rvt != null) {
-            String str = rvt.toString();
-            if (str.length() > 200) {
-                str = str.substring(0, 200) + "...";
-            }
-
-            log.info("return 返回值:");
-            log.info(str);
-        }
-
-        log.info("耗时（毫秒） : " + (System.currentTimeMillis() - startTime.get()));
-
-        log.info("========================= after -- end =========================");
     }
 }
